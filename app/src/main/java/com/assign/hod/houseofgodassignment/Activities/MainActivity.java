f8f21f383c17cff07cba9812c415d37bb84f4e59
indolia143 <indolia143@gmail.com>
2018-12-12 01:02:01 +0530

    Initial commith

--- app/src/main/java/com/assign/hod/houseofgodassignment/Activities/MainActivity.java
+++ app/src/main/java/com/assign/hod/houseofgodassignment/Activities/MainActivity.java
@@ -53,6 +53,7 @@ public class MainActivity extends AppCompatActivity  {
                 //mAdapter.notifyDataSetChanged();
 
                 if(Constant.pos==0) {
+
                     Constant.pos = pos;
                     mRecyclerView.getAdapter().notifyItemChanged(pos);
                 }else{
