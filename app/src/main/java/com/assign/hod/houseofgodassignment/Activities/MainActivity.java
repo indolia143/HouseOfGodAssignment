71e471560415a301be178aaa479e7afe0c7cff5a
indolia143 <indolia143@gmail.com>
2018-12-12 22:19:16 +0530

    Initial commith

--- app/src/main/java/com/assign/hod/houseofgodassignment/Activities/MainActivity.java
+++ app/src/main/java/com/assign/hod/houseofgodassignment/Activities/MainActivity.java
@@ -59,6 +59,7 @@ public class MainActivity extends AppCompatActivity  {
                 }else{
                     Constant.pos = pos;
                     mAdapter.notifyDataSetChanged();
+
                 }
 
             }
